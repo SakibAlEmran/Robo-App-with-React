c4c96c3cc2ea97657be6d8e42e23dcc88a59e281
Sakib Al Emran <sakib.imran0909@gmail.com>
2023-01-16 21:55:25 +0200

    updated cardlist component with user object

--- src/index.js
+++ src/index.js
@@ -3,15 +3,13 @@ import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 
 import 'tachyons';
-import Card from './Card';
+import CardList from './CardList';
 import { robots } from './robots';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+    <CardList robots={robots}/>
   </React.StrictMode>
 );
 
