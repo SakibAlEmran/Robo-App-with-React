3a401b8b0ebe5e65d0e1a7fb835c59ae96eb2086
Sakib Al Emran <sakib.imran0909@gmail.com>
2023-01-21 23:12:30 +0200

    added scroll option and keeping the search box on top

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import CardList from "./Components/CardList";
+import SearchBox from "./Components/SearchBox";
+import Scroll from "./Components/Scroll";
+
+
+class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            robots: [],
+            searchField: '',
+        }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({robots: users}))
+    }
+
+    onChangeSearch = (event) => {
+        this.setState({searchField: event.target.value});
+        console.log(event.target.value);
+    }
+
+    render(){
+        const {robots, searchField} = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());
+        })
+
+        if(!robots.length){
+            return(
+                <h1> Loading </h1>
+            )
+        }else{
+            return (
+                <div className="tc">
+                    <h1>Robo Friends</h1>
+                    <SearchBox searchChange={this.onChangeSearch}/>
+                    <Scroll>
+                        <CardList robots={filteredRobots}/>
+                    </Scroll>
+                </div>
+            )
+        }
+    }
+}
+
+export default App;
